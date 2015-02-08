a41c3bc0985390195eb2c5dfd50a3e61b8503dad
Lance R. Vick <lance@lrvick.net>
2015-02-07 16:33:28 -0800

    add initial config via editing etcfile vs attempting to fire up daemon

--- run.sh
+++ run.sh
@@ -34,7 +34,6 @@ EOF
 
   dpkg-reconfigure -f noninteractive slapd
 
-  slapd -h "ldapi:///" -u openldap -g openldap 
   chown -R openldap:openldap /etc/ldap 
 
   if [ -e /etc/ldap/ssl/ldap.crt ] && \
@@ -43,35 +42,11 @@ EOF
   
     echo "SSL Certificates Found"
 
-    chmod 600 /etc/ldap/ssl/ldap.key
-
-    if [ -e /etc/ldap/ssl/dhparam.pem ]; then
-      echo "Generating /etc/ldap/ssl/dhparam.pem"
-      openssl dhparam -out /etc/ldap/ssl/dhparam.pem 2048
-    fi
-    
-    ldapmodify -Y EXTERNAL -H ldapi:/// -Q <<EOF
-
-dn: cn=config
-changetype: modify
-replace: olcTLSCACertificateFile
-olcTLSCACertificateFile: /etc/ldap/ssl/ca.crt
--
-replace: olcTLSCertificateFile
-olcTLSCertificateFile: /etc/ldap/ssl/ldap.crt
--
-replace: olcTLSCertificateKeyFile
-olcTLSCertificateKeyFile: /etc/ldap/ssl/ldap.key
--
-replace: olcTLSDHParamFile
-olcTLSDHParamFile: /etc/ldap/ssl/dhparam.pem
--
-replace: olcTLSVerifyClient
-olcTLSVerifyClient: never
-
+    echo <<EOF >> /etc/ldap/slapd.conf
+TLSCACertificateFile    /etc/ldap/ssl/ca.crt
+TLSCertificateKeyFile   /etc/ldap/ssl/ldap.key
+TLSCertificateFile      /etc/ldap/ssl/ldap.crt
 EOF
-  
-    killall slapd
 
   fi
 
