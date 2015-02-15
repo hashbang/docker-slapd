0b9139c33a40d2bd97939750bd6ca5be030f99b9
Lance R. Vick <lance@lrvick.net>
2015-02-14 20:30:34 -0800

    run config on every run

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF
 
-SlapD Config:
+Setting SlapD Config:
 
 ROOTPASS=${ROOTPASS}
 DOMAIN=${DOMAIN}
@@ -10,16 +10,14 @@ ORG=${ORG}
 
 EOF
 
-if [ ! -e /var/lib/ldap/.bootstrapped ]; then
-
-  cat <<EOF | debconf-set-selections
+cat <<EOF | debconf-set-selections
 slapd slapd/internal/generated_adminpw password ${ROOTPASS}
 slapd slapd/internal/adminpw password ${ROOTPASS}
 slapd slapd/password2 password ${ROOTPASS}
 slapd slapd/password1 password ${ROOTPASS}
-slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
 slapd slapd/domain string ${DOMAIN}
 slapd shared/organization string ${ORG}
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
 slapd slapd/backend string HDB
 slapd slapd/purge_database boolean true
 slapd slapd/move_old_database boolean true
@@ -28,23 +26,19 @@ slapd slapd/no_configuration boolean false
 slapd slapd/dump_database select when needed
 EOF
 
-  dpkg-reconfigure -f noninteractive slapd
+dpkg-reconfigure -f noninteractive slapd
 
-  if [ -e /etc/ldap/ssl/ldap.crt ] && \
-     [ -e /etc/ldap/ssl/ldap.key ] && \
-     [ -e /etc/ldap/ssl/ca.crt ]; then
-  
-    echo "SSL Certificates Found"
+if [ -e /etc/ldap/ssl/ldap.crt ] && \
+   [ -e /etc/ldap/ssl/ldap.key ] && \
+   [ -e /etc/ldap/ssl/ca.crt ]; then
 
-    echo <<EOF >> /etc/ldap/slapd.conf
+  echo "SSL Certificates Found"
+
+  echo <<EOF >> /etc/ldap/slapd.conf
 TLSCACertificateFile    /etc/ldap/ssl/ca.crt
 TLSCertificateKeyFile   /etc/ldap/ssl/ldap.key
 TLSCertificateFile      /etc/ldap/ssl/ldap.crt
 EOF
-
-  fi
-
-  touch /var/lib/ldap/.bootstrapped
 
 fi
 
