c45993cc6c876a5230aff74b8f16d597b1634a5e
Lance R. Vick <lance@lrvick.net>
2015-04-04 23:19:31 -0700

    adjust formatting, fix type

--- run.sh
+++ run.sh
@@ -15,23 +15,23 @@ EOF
     ulimit -n 1024
 
     cat <<- EOF | debconf-set-selections
-    slapd   slapd/internal/generated_adminpw password ${ROOTPASS}
-    slapd   slapd/internal/adminpw passwod ${ROOTPASS}
-    slapd   slapd/password1 password ${ROOTPASS}
-    slapd   slapd/password2 password ${ROOTPASS}
-    slapd   slapd/domain    string  ${DOMAIN}
-    slapd   shared/organization string ${ORG}
-    slapd   slapd/unsafe_selfwrite_acl  note    
-    slapd   slapd/invalid_config    boolean true
-    slapd   slapd/no_configuration  boolean false
-    slapd   slapd/purge_database    boolean true
-    slapd   slapd/allow_ldap_v2 boolean false
-    slapd   slapd/upgrade_slapcat_failure   error   
-    slapd   slapd/password_mismatch note    
-    slapd   slapd/backend   select  HDB
-    slapd   slapd/dump_database select  when needed
-    slapd   slapd/dump_database_destdir string  /var/backups/slapd-VERSION
-    slapd   slapd/move_old_database boolean true
+    slapd slapd/internal/generated_adminpw password ${ROOTPASS}
+    slapd slapd/internal/adminpw           password ${ROOTPASS}
+    slapd slapd/password1                  password ${ROOTPASS}
+    slapd slapd/password2                  password ${ROOTPASS}
+    slapd slapd/domain                     string   ${DOMAIN}
+    slapd shared/organization              string   ${ORG}
+    slapd slapd/unsafe_selfwrite_acl       note
+    slapd slapd/invalid_config             boolean  true
+    slapd slapd/no_configuration           boolean  false
+    slapd slapd/purge_database             boolean  true
+    slapd slapd/allow_ldap_v2              boolean  false
+    slapd slapd/upgrade_slapcat_failure    error
+    slapd slapd/password_mismatch          note
+    slapd slapd/backend select             HDB
+    slapd slapd/dump_database              select   when needed
+    slapd slapd/dump_database_destdir      string   /var/backups/slapd-VERSION
+    slapd slapd/move_old_database          boolean  true
 EOF
 
     dpkg-reconfigure -f noninteractive slapd
